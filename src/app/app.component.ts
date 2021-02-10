816fab87ebd6621a2de930215f0d37e637a6a997
MaksymSlynko <maksok1502@gmail.com>
2021-02-10 15:12:34 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'formsStudy';
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Rob', 'rob@test.com', 5213123213, '', 'morning', true);
 }
